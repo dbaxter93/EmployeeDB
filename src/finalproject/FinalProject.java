21877cacc453b733b806ea9277aab0e94f4b6bfc
dbaxter93 <drewbaxter93@outlook.com>
2022-06-21 16:53:17 -0500

    Add files via upload

---
+++ src/finalproject/FinalProject.java
@@ -0,0 +1,12 @@
+package finalproject;
+
+import FinalView.EmployeeGUI;
+
+public class FinalProject {
+
+    public static void main(String[] args) {
+        new EmployeeGUI();
+        // I want to clear the fields in EmployeeGUI after successfully inserting or deleting an employee.
+    }
+    
+}
